2a8819e351ff370e805fd3e3a742162b72f8bfe6
daniel watson <ozzloy@each.do>
2024-12-16 12:40:58 -0800

    configure eslint with prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { ignores: ["dist"] },
@@ -28,6 +29,7 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      ...eslintConfigPrettier.rules, // Add Prettier rules last to override conflicting rules
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
