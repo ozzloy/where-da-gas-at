62ca708dbfcb329b7eef0d8f34f603760a4128e8
Sqhy4445 <nchen093@gmail.com>
2024-12-18 13:23:37 -0500

    create migration

--- app/models/review.py
+++ app/models/review.py
@@ -2,27 +2,27 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
 class Review(db.Model):
-    __tablename__ = "reviews"
+    __tablename__ = "review"
 
     if environment == "production":
         __table_args__ = {"schema": SCHEMA}
 
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
-    station_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("stations.id")), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("user.id")), nullable=False)
+    # station_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("station.id")), nullable=False)
     review = db.Column(db.Text, nullable=False)
 
-    user = db.relationship("User", back_populates="reviews")
-    station = db.relationship("Station", back_populates="reviews")
+    user = db.relationship("User", back_populates="reviews", cascade="all, delete-orphan")
+    # station = db.relationship("Station", back_populates="reviews", cascade="all, delete-orphan")
 
 
 
-def to_dic(self):
+def to_dict(self):
     return {
         "id": self.id,
         "user_id": self.user_id,
-        "station_id": self.station_id,
+        # "station_id": self.station_id,
         "review": self.review
 
     }
