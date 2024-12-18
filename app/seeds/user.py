d977ae1a17080db3297b8f62254353b1a29c505d
Matthew Nicholson <thematthewnicholson@gmail.com>
2024-12-18 12:10:22 -0800

    init branch

--- app/seeds/user.py
+++ app/seeds/user.py
@@ -26,7 +26,7 @@ def seed_user():
 # and RESET IDENTITY resets the auto incrementing primary key, CASCADE
 # deletes any dependent entities. With sqlite3 in development you need
 # to instead use DELETE to remove all data and it will reset the
-# primary keys for you as well.
+# primary keys for you as well
 def undo_user():
     if environment == "production":
         db.session.execute(
