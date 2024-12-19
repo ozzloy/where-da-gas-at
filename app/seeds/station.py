20bfda90e435e3b8495a53f3a4cad365f210bb85
daniel watson <ozzloy@each.do>
2024-12-18 16:31:31 -0800

    use list in station seed to be robust against id sequence changes

--- app/seeds/station.py
+++ app/seeds/station.py
@@ -1,42 +1,53 @@
-from app.models import db, Station, environment, SCHEMA
 from sqlalchemy.sql import text
+
+from app.models import db, Station, environment, SCHEMA
+from app.models.user import User
+from .user import user_seeds
+
+
+emails = [user_seed["email"] for user_seed in user_seeds]
+
+station_seeds = [
+    {
+        "name": "Electric Vehicle Charging Station",
+        "lat": 47.5832543,
+        "lng": -122.03377579999999,
+        "address": "3020 Issaquah-Pine Lake Rd SE, Sammamish, WA 98075, USA",
+        "uri": "https://maps.google.com/?cid=90028760738209365",
+        "location_id": "ChIJuwnmD-ZvkFQRVZ5eT67YPwE",
+    },
+    {
+        "name": "Shell",
+        "lat": 47.615336899999996,
+        "lng": -122.0374069,
+        "address": "22631 NE Inglewood Hill Rd, Sammamish, WA 98074, USA",
+        "uri": "https://maps.google.com/?cid=1862383325217865551",
+        "location_id": "ChIJ6atYSPRxkFQRT784V8SD2Bk",
+    },
+    {
+        "name": "Tesla Supercharger",
+        "lat": 47.6164997,
+        "lng": -122.0332938,
+        "address": "430-710 228th Ave NE, Sammamish, WA 98074, USA",
+        "uri": "https://maps.google.com/?cid=4681353172202683726",
+        "location_id": "ChIJ6atYSPRxkFQRT784V8SD2Bk",
+    },
+]
 
 
 def seed_station():
-    first = Station(
-        id=1,
-        name="Electric Vehicle Charging Station",
-        lat=47.5832543,
-        lng=-122.03377579999999,
-        address="3020 Issaquah-Pine Lake Rd SE, Sammamish, WA 98075, USA",
-        uri="https://maps.google.com/?cid=90028760738209365",
-        location_id="ChIJuwnmD-ZvkFQRVZ5eT67YPwE",
-        user_id=1,
-    )
-    second = Station(
-        id=2,
-        name="Shell",
-        lat=47.615336899999996,
-        lng=-122.0374069,
-        address="22631 NE Inglewood Hill Rd, Sammamish, WA 98074, USA",
-        uri="https://maps.google.com/?cid=1862383325217865551",
-        location_id="ChIJ6atYSPRxkFQRT784V8SD2Bk",
-        user_id=2,
-    )
-    third = Station(
-        id=3,
-        name="Tesla Supercharger",
-        lat=47.6164997,
-        lng=-122.0332938,
-        address="430-710 228th Ave NE, Sammamish, WA 98074, USA",
-        uri="https://maps.google.com/?cid=4681353172202683726",
-        location_id="ChIJ6atYSPRxkFQRT784V8SD2Bk",
-        user_id=3,
-    )
-
-    db.session.add(first)
-    db.session.add(second)
-    db.session.add(third)
+    users = User.query.filter(User.email.in_(emails)).all()
+    for user, station_seed in zip(users, station_seeds):
+        station = Station(
+            name=station_seed["name"],
+            lat=station_seed["lat"],
+            lng=station_seed["lng"],
+            address=station_seed["address"],
+            uri=station_seed["uri"],
+            location_id=station_seed["location_id"],
+            user_id=user.id,
+        )
+        db.session.add(station)
     db.session.commit()
 
 
