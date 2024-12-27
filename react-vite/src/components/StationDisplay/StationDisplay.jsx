1d6121cf8c86389b49714922f4aee4b9d1081232
glowupmatt <glowupmatt@gmail.com>
2024-12-27 08:38:29 -0800

    create the station display page

--- react-vite/src/components/StationDisplay/StationDisplay.jsx
+++ react-vite/src/components/StationDisplay/StationDisplay.jsx
@@ -1,13 +1,33 @@
-import { useParams } from "react-router-dom"
-import { useGetSelectedStation } from "../../hooks/useGetSelectedStation"
+import { useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { GoogleMapContext } from "../../context/GoogleMapContext";
+import { useGetSelectedStation } from "../../hooks/useGetSelectedStation";
+import "./StationDisplay.css";
+import StationImageDisplay from "./StationDataComponents/StationImageDisplay";
+import PriceOptionsDisplay from "./StationDataComponents/PriceOptionsDisplay";
 
 function StationDisplay() {
-    const { id } = useParams()
-    const stationInfo = useGetSelectedStation({id})
-    console.log(stationInfo)
+    const { id } = useParams();
+    const stationInfo = useGetSelectedStation({ id });
+    const { setSelectedStation } = useContext(GoogleMapContext);
+    useEffect(() => {
+        if (stationInfo) {
+        setSelectedStation(stationInfo);
+        }
+    }, [stationInfo, setSelectedStation]);
+
+    if (!stationInfo) return <h1>Loading...</h1>;
+
     return (
-        <div>StationDisplay</div>
-    )
+        <section className="station-display-main-container">
+            <StationImageDisplay />
+            <div className="station-data-container">
+                <h1>{stationInfo.displayName.text}</h1>
+                <p>{stationInfo.formattedAddress}</p>
+                <PriceOptionsDisplay stationInfo={ stationInfo } />
+            </div>
+        </section>
+    );
 }
 
 export default StationDisplay;
