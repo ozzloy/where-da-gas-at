888a28638a7b1bbce5cea550eb08552efe16382f
glowupmatt <glowupmatt@gmail.com>
2024-12-27 08:38:08 -0800

    create utils folder and change X-Goog-FieldMask to display all fields

--- react-vite/src/hooks/useGetNearByStations.jsx
+++ react-vite/src/hooks/useGetNearByStations.jsx
@@ -18,8 +18,7 @@ function useGetNearByStations({ center }) {
           headers: {
             "Content-Type": "application/json",
             "X-Goog-Api-Key": API_KEY,
-            "X-Goog-FieldMask":
-              "places.accessibilityOptions,places.addressComponents,places.adrFormatAddress,places.attributions,places.businessStatus,places.containingPlaces,places.displayName,places.formattedAddress,places.googleMapsLinks,places.googleMapsUri,places.iconBackgroundColor,places.iconMaskBaseUri,places.id,places.location,places.name,places.photos,places.plusCode,places.primaryType,places.primaryTypeDisplayName,places.pureServiceAreaBusiness,places.shortFormattedAddress,places.rating,places.subDestinations,places.types,places.utcOffsetMinutes,places.viewport,places.priceLevel",
+            "X-Goog-FieldMask": "*",
           },
           body: JSON.stringify({
             locationRestriction: {
---
+++ react-vite/utils/convertNanosToUSD.js
@@ -0,0 +1,5 @@
+export function convertNanosToUSD(units, nanos) {
+    const fractionalDollars = nanos / 1_000_000_000;
+    const totalPrice = parseFloat(units) + fractionalDollars;
+    return totalPrice.toFixed(2); // Format to 2 decimal places
+  }
