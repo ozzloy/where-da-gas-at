7e3c1a08d08250feb5b09ce0f0b2ab41c4a3124e
daniel watson <ozzloy@each.do>
2024-12-31 07:56:28 -0800

    separate user and station creation into functions

--- tests/api/auth.py
+++ tests/api/auth.py
@@ -18,3 +18,54 @@ def create_authenticated_session() -> requests.Session:
         raise Exception("failed to authenticate")
 
     return session
+
+
+def create_user(session: requests.Session):
+    """
+    create a brand new user in db
+
+    given a session that already has csrf and session cookies
+    return dict with fields filled with random values
+    """
+    user = make_user()
+    password = user["password"]
+    signup_response = session.post(
+        get_full_url("auth/signup"), json=user
+    )
+    validate_response(signup_response)
+
+    state = signup_response.json()
+    validate_state(state, ["user"])
+
+    user_slice = state["user"]
+    if len(user_slice) != 1:
+        raise Exception(f"{len(user_slice) = }")
+
+    user = next(iter(user_slice.values()))
+    user["password"] = password
+    return user
+
+
+def create_station(session: requests.Session, user_id: int):
+    """
+    create a station in db
+
+    given session that is authenticated
+    return station dict with fields filled with random values
+    """
+
+    station = make_station(user_id)
+    create_station_response = session.post(
+        get_full_url("station"), json=station
+    )
+    validate_response(create_station_response)
+
+    state = create_station_response.json()
+    validate_state(state, ["station"])
+
+    station_slice = state["station"]
+    if len(station_slice) != 1:
+        raise Exception(f"{len(station_slice) = }")
+
+    station = next(iter(station_slice.values()))
+    return station
