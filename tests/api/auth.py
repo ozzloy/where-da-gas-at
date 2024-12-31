201452746330c9f7ca1d2ff456a04bad0a68d3d2
daniel watson <ozzloy@each.do>
2024-12-31 07:51:07 -0800

    remove unused, mispeled function

--- tests/api/auth.py
+++ tests/api/auth.py
@@ -18,14 +18,3 @@ def create_authenticated_session() -> requests.Session:
         raise Exception("failed to authenticate")
 
     return session
-
-
-def get_user_id_from_resposne(response: Dict[str, Any]) -> int:
-    try:
-        user = response["user"]
-        user_id = next(iter(user_data.values()))["id"]
-        return user_id
-    except (KeyError, StopIteration) as e:
-        raise ValueError(
-            f"could not extract user id from response: {e}"
-        )
