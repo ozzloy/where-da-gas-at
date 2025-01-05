99a461902203c81040812391dcb38a2b2811d1f7
Sqhy4445 <nchen093@gmail.com>
2025-01-04 23:17:10 -0500

    display station name in lieu of station id

--- react-vite/src/components/GoogleMapsDisplay/ControlCRUDComponents/UserReviewsDisplay.jsx
+++ react-vite/src/components/GoogleMapsDisplay/ControlCRUDComponents/UserReviewsDisplay.jsx
@@ -1,11 +1,27 @@
 import { ReviewContext } from "../../../context/UserReviewContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "./UserReviewDisplay.css";
 
 export default function UserReviewsDisplay() {
   const { reviews, loading, error } = useContext(ReviewContext);
   const sessionUser = useSelector((store) => store.session.user);
+  const [station, setStation] = useState({});
+
+  useEffect(() => {
+    async function fetchStation() {
+      const res = await fetch(`/api/station/`);
+      const data = await res.json();
+
+      console.log(data);
+
+      if (!res.ok) {
+        throw new Error("Failed to get stations");
+      }
+      setStation(data["station"]);
+    }
+    fetchStation();
+  }, []);
 
   if (loading) return <p>Loading reviews...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -14,15 +30,20 @@ export default function UserReviewsDisplay() {
   }
 
   return (
-    <div>
-      <h2>User Reviews</h2>
+    <div className="user-review-container">
+      <h2>Reviewer: {sessionUser.name}</h2>
       <ul>
-        {reviews // reviews
-          .filter((review) => review.id === sessionUser.id)
+        {reviews
+          .filter((review) => review.king_id === sessionUser.id)
           .map((review) => (
             <li key={review.id}>
-              <p>{review.text}</p>
-              <p>Station ID: {review.station_id}</p>
+              <p>
+                Station:{" "}
+                {review.station_id in station
+                  ? station[review.station_id].name
+                  : review.station_id}
+              </p>
+              <p>Review: {review.text}</p>
             </li>
           ))}
       </ul>
