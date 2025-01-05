6b483bcc01a47994f0a21ec08010f0512918a00e
Sqhy4445 <nchen093@gmail.com>
2025-01-04 23:22:26 -0500

    remove unused code

--- react-vite/src/components/GoogleMapsDisplay/GoogleMapsInfoDisplay/SaveSpotIcon.jsx
+++ react-vite/src/components/GoogleMapsDisplay/GoogleMapsInfoDisplay/SaveSpotIcon.jsx
@@ -1,12 +1,11 @@
 import "./SaveSpotIcon.css";
 import { FaBookmark } from "react-icons/fa";
-import { useSelector } from "react-redux";
 import OpenModalMenuItem from "../../Navigation/OpenModalMenuItem";
 import SaveSpotConfirmation from "./SaveSpotConfirmation";
 
 function SaveSpotIcon() {
   // NEED TO ADD FUNCTIONALITY TO SAVE THE LOCATION TO THE DATABASE
-  const sessionUser = useSelector((store) => store.session.user);
+
   return (
     <div>
       <OpenModalMenuItem
