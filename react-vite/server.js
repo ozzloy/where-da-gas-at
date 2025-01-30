0097066aa350c3af867f5767b5095ac3c4d1b5c2
daniel watson <ozzloy@each.do>
2025-01-30 08:44:06 -0800

    switch frontend to port 3002 or whatever is in .env

--- react-vite/server.js
+++ react-vite/server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // serve static files from the dist directory
 app.use(express.static(join(__dirname, "dist")));
