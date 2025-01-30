30047c6d6925775e3f28ffdde440673676e80c36
daniel watson <ozzloy@each.do>
2025-01-29 18:22:48 -0800

    make a server for frontend for deployment

---
+++ react-vite/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 3001;
+
+// serve static files from the dist directory
+app.use(express.static(join(__dirname, "dist")));
+
+// all other requests get index.html
+app.get("*", (_req, res) => {
+  res.sendFile(join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
