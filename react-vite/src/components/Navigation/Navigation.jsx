47c3531a61d2b38d8656dd988eb13633f42f7979
daniel watson <ozzloy@each.do>
2025-01-30 16:41:00 -0800

    remove light/dark toggle button

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -3,12 +3,14 @@ import { GoogleMapContext } from "../../context/GoogleMapContext";
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-import DarkLightMode from "../DarkLightModeDisplay/DarkLightMode";
+// import DarkLightMode from "../DarkLightModeDisplay/DarkLightMode";
 import { ThemeContext } from "../../context/ThemeContext";
 
 function Navigation() {
   const { openSideMenu } = useContext(GoogleMapContext);
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  const {
+    theme, // , toggleTheme
+  } = useContext(ThemeContext);
 
   if (!openSideMenu) {
     return (
@@ -18,9 +20,6 @@ function Navigation() {
             <img src="/Logo.ico" className="logo" alt="logo" />
             <p>Where da gas at?</p>
           </NavLink>
-        </li>
-        <li>
-          <DarkLightMode theme={theme} toggleTheme={toggleTheme} />
         </li>
         <li>
           <ProfileButton location="navigation-header" />
